b37f93ff08101e02d6bd3cb8af428bdb8d10b9d8
Marcelo rentería <u202216558@upc.edu.pe>
2025-06-04 19:48:50 -0500

    Initial commit Note: for compose.yaml, the app image is the image ID after building.

---
+++ src/main/java/com/inncontrol/platform/inncontroltaskservice/InncontrolTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inncontrol.platform.inncontroltaskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InncontrolTaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InncontrolTaskServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inncontrol/platform/inncontroltaskservice/InncontrolTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inncontrol.platform.inncontroltaskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InncontrolTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
